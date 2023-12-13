df2076e533fe06aefca43f9bdfd5221f305a1fa1
Devankit2022 <devankit2022@gmail.com>
2023-12-13 08:15:10 +0530

    create dec-2023

---
+++ src/constants/images.js
@@ -0,0 +1,23 @@
+import email from "../assets/email.png"
+import mobile from "../assets/mobile.png"
+
+import javascript from "../assets/javascript.png"
+import react from "../assets/react.png"
+import eth from "../assets/eth.png"
+
+import profile from "../assets/profile.png"
+import circle from "../assets/circle.svg"
+import logo from "../assets/logo-removebg.png"
+
+const images = {
+	email,
+	mobile,
+	javascript,
+	react,
+	eth,
+	profile,
+	circle,
+	logo,
+}
+
+export default images
